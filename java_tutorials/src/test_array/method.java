407a63ca78b2e9a9662aa60c2a42db04e04003ce
estellechoi <estele.choi@gmail.com>
2019-05-21 16:38:44 +0900

    Structure of method, Method overloading

---
+++ java_tutorials/src/test_array/method.java
@@ -0,0 +1,19 @@
+package test_array;
+
+public class method {
+	
+	// 접근자 public, private, default, protected(같은 패키지, 상속시에만 허용)
+	// * default 접근자는 생략 가능 !
+	// 리턴타입 (void or 리턴값 자료형)
+	// 메소드명(매개변수) { return값}
+	
+
+	public int cal(int x) {
+		x=x+1;
+		
+		return x;
+	}
+	
+	
+
+}
---
+++ java_tutorials/src/test_method/Method.java
@@ -0,0 +1,25 @@
+package test_method;
+
+public class Method { // Class means a program.
+	
+	int kor; // variable or array
+	
+	// constructor omitted
+	
+	public void aa() { // method
+		
+		bb();
+		cc();
+		
+		System.out.println("aa");
+	}
+	
+	public void bb() {
+		System.out.println("bb");
+	}
+	
+	public void cc() {
+		System.out.println("cc");
+	}
+
+}

53c5b8853743712caf7f9b8f71a564323b73fbf6
estellechoi <estele.choi@gmail.com>
2019-05-23 17:05:33 +0900

    the concept of parameter and argument (매개변수, 전달인자)

--- java_tutorials/src/test_method/Extends1_main.java
+++ java_tutorials/src/test_method/Extends1_main.java
@@ -8,7 +8,9 @@ public class Extends1_main {
 		
 		// 부모 생성자의 매개변수 int age 초기값을,
 		// 자식 클래스 복사시에 int par 값으로 넣어줌
+		
 		Extends1_son s1 = new Extends1_son(60, 27);
+		// 매개변수에 들어가는 60, 27은 arguments 라 하는듯 ?
 		
 
 	}

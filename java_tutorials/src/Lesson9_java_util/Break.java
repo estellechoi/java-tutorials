9e48d9c7080ea63f682bd8c9da2abc837946f4c3
estellechoi <estele.choi@gmail.com>
2019-06-10 12:06:50 +0900

    new Random(); new Date(); Calendar.getInstance();

---
+++ java_tutorials/src/Lesson9_java_util/Break.java
@@ -0,0 +1,25 @@
+package Lesson9_java_util;
+
+public class Break {
+
+	public static void main(String[] args) {
+		
+		for(int i=0; i<=10; i++) {
+			System.out.println(i+ " test");
+			
+			if(i == 1)
+				break; // 여기서 for문을 종료하라
+			System.out.println(i+ " test +");
+		}
+		
+		System.out.println(" ");
+		
+		for(int i=0; i<10; i++) {
+			if(!(i%2 == 0))
+				continue; // 여기서 1회전 종료하고 다음 회전을 시작하라
+			System.out.println(i);
+		}
+
+	}
+
+}
---
+++ java_tutorials/src/Lesson9_java_util/util_Random_nextInt.java
@@ -0,0 +1,31 @@
+package Lesson9_java_util;
+import java.util.Random;
+public class util_Random_nextInt {
+
+	public static void main(String[] args) {
+		
+		// 시작 : 1020  끝 : 1099
+		// int x = ra.nextInt("범위") + "시작값";
+		
+		Random ra = new Random();
+		
+		int lotto = ra.nextInt(80) + 1020;
+		System.out.println(lotto);
+		
+		// * 무한반복
+		// for(int i=0; i<=0;) or for(;;)
+		// while(true)
+		int xx;
+		int n = 0;
+		
+		for(;;) {
+			xx = ra.nextInt(80)+1020; // (0 ~ 79) + 1020
+			if(xx==1079)
+				break;
+			n++;
+		}
+		System.out.println(n+" 번째 → "+xx);
+
+	}
+
+}

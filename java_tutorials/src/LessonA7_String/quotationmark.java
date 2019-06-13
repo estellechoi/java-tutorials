bf2ba42dca6afdb2aa08fdaa525435f8202f4767
estellechoi <estele.choi@gmail.com>
2019-06-13 15:44:08 +0900

    quotation mark training

---
+++ java_tutorials/src/LessonA7_String/quotationmark.java
@@ -0,0 +1,21 @@
+package LessonA7_String;
+
+public class quotationmark {
+
+	public static void main(String[] args) {
+		
+		// 학교종이 '땡땡땡' 어서모이자 '선생님'이 '우리'를 기다리신다.
+		String a = "땡땡땡";
+		String b = "선생님";
+		String c = "우리";
+		
+		String sql="학교종이 '"+a+"' 어서모이자 '"+b+"'이 '"+c+"'를 기다리신다.";
+		System.out.println(sql);
+		
+		// 이 문장에서 ''는 기능이 없고 학,교,종,이 와 같은 문자이다 !
+		
+		
+
+	}
+
+}

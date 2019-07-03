42f4eed4512d21674590f6d1064c85e41633d274
estellechoi <estele.choi@gmail.com>
2019-07-03 13:08:13 +0900

    java.awt.TextArea; TextArea.setLineWrap(true); 자동줄바꿈 메소드

---
+++ java_tutorials/src/LessonB4_Windows/awt_TextArea.java
@@ -0,0 +1,24 @@
+package LessonB4_Windows;
+import javax.swing.*;
+import java.awt.*;
+public class awt_TextArea extends JFrame {
+
+	TextArea ta = new TextArea("입력하시오", 5, 30);
+	JTextArea jta = new JTextArea("입력하시오", 5, 30);
+
+	awt_TextArea() {
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		setBounds(300,300,400,400);
+		
+		setLayout(new FlowLayout());
+		
+		// 자동줄바꿈 메소드
+		jta.setLineWrap(true);
+		
+		add(ta);
+		add(jta);
+	}
+
+}
---
+++ java_tutorials/src/LessonB4_Windows/awt_TextArea_main.java
@@ -0,0 +1,10 @@
+package LessonB4_Windows;
+
+public class awt_TextArea_main {
+
+	public static void main(String[] args) {
+		
+		awt_TextArea f= new awt_TextArea();
+	}
+
+}

a4197a88b3e88b580ee93bf9998900ed8af77993
estellechoi <estele.choi@gmail.com>
2019-07-08 11:22:39 +0900

    인터페이스 개념 복습

---
+++ java_tutorials/src/LessonB5_EventListener/test.java
@@ -0,0 +1,22 @@
+package LessonB5_EventListener;
+
+// Interface
+interface ToBeImplemented {
+	
+	// 추상 메소드
+	public void print();
+	public void input();
+}
+
+// 구현클래스 Implementing class
+public class test implements ToBeImplemented {
+	
+	// 메소드 오버라이딩 (메소드 재정의)
+	public void print() {
+		
+	}
+	
+	public void input() {
+		
+	}
+}

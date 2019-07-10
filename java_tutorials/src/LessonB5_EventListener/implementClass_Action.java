5ce614aaf79b3735f3dea00378bc72cfa4ea7db5
estellechoi <estele.choi@gmail.com>
2019-07-10 10:07:51 +0900

    Object ob = e.getSource(); Object ob를 통해 클릭된 버튼객체에 기능 정의하기

--- java_tutorials/src/LessonB5_EventListener/implementClass_Action.java
+++ java_tutorials/src/LessonB5_EventListener/implementClass_Action.java
@@ -9,22 +9,33 @@ import java.awt.*;
 public class implementClass_Action extends JFrame implements ActionListener {
 
 	Button btn = new Button("닫기");
+	Button btn2 = new Button("실행");
 
 	implementClass_Action() {
 		add(btn);
+		add(btn2);
 		
-		// * add ActionListener on this Button. (현재 창에 이벤트 추가)
+		// * add ActionListener on this Button. (this=btn 객체(버튼)에 이벤트 장착)
 		// onclick = "ActionListener(this)"
 		btn.addActionListener(this); 
-
+		btn2.addActionListener(this);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(200,200);
 		setLayout(new FlowLayout());
 		setVisible(true);
 	}
 
+	@Override
 	public void actionPerformed(ActionEvent e) {
-		System.out.println("프로그램 종료");
-		System.exit(0);
+		// System.exit(0);
+		// * 클릭된 버튼객체가 리턴된 객체 Object ob를 통해 각 객체에 기능 정의하기
+		Object ob = e.getSource(); 
+		if (ob == btn) {
+			setTitle("닫기");
+		} else if (ob == btn2) {
+			setTitle("실행");
+		}
 	}
 
 }

2776a74015d6d52009392b0c7425b21021097860
estellechoi <estele.choi@gmail.com>
2019-07-15 14:37:20 +0900

    Object ob = e.getSource();

--- java_tutorials/src/LessonB5_EventListener/implementClass_Action3.java
+++ java_tutorials/src/LessonB5_EventListener/implementClass_Action3.java
@@ -6,32 +6,44 @@ import java.awt.event.*;
 
 public class implementClass_Action3 extends JFrame implements ActionListener {
 
+	// text 입력양식
 	TextField num1 = new TextField(4);
 	TextField num2 = new TextField(4);
+	TextField result = new TextField(4);
+	// text
+	Label id = new Label("=");
+	// button
 	Button btn1 = new Button("합");
 	Button btn2 = new Button("곱");
 	Button btn3 = new Button("나");
 	Button btn4 = new Button("뺌");
-	TextField result = new TextField(4);
+	Button btn5 = new Button("a~b");
+	Button btn6 = new Button("LOTTO");
 
 	implementClass_Action3() {
 
+		// eventListener 장착
 		btn1.addActionListener(this);
 		btn2.addActionListener(this);
 		btn3.addActionListener(this);
 		btn4.addActionListener(this);
+		btn5.addActionListener(this);
+		btn6.addActionListener(this);
 
-		// 배치
+		// frame에 배치
 		add(num1);
-		add(num2);
 		add(btn1);
 		add(btn2);
 		add(btn3);
 		add(btn4);
+		add(btn5);
+		add(btn6);
+		add(num2);
+		add(id);
 		add(result);
 
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(400, 100);
+		setSize(600, 100);
 		setLayout(new FlowLayout());
 		setVisible(true);
 	}
@@ -42,23 +54,30 @@ public class implementClass_Action3 extends JFrame implements ActionListener {
 		// 입력값 가져오기
 		int n1 = Integer.parseInt(num1.getText());
 		double n2 = Integer.parseInt(num2.getText());
-		Integer res = 0;
+		Integer resultValue = 0;
 
 		// 클릭된 버튼객체 가져오기
 		Object ob = e.getSource();
 
 		// 계산
 		if (ob == btn1) {
-			res = (int) (n1 + n2);
+			resultValue = (int) (n1 + n2);
 		} else if (ob == btn2) {
-			res = (int) (n1 * n2);
+			resultValue = (int) (n1 * n2);
 		} else if (ob == btn3) {
-			res = (int) (n1 / n2);
+			resultValue = (int) (n1 / n2);
 		} else if (ob == btn4) {
-			res = (int) (n1 - n2);
+			resultValue = (int) (n1 - n2);
+		} else if (ob == btn5) {
+			for (int i = n1; i <= (int) n2; i++) {
+				resultValue = resultValue + i;
+			}
+		} else if (ob == btn6) {
+			resultValue = (int) (Math.random() * ((int) n2 - n1 + 1) + n1);
 		}
+		// * Math.random() : 0 이상 ~ 1 미만
 
-		result.setText(res.toString());
+		result.setText(resultValue.toString());
 
 	}
 }

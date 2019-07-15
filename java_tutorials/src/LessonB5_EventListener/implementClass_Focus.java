49cb54a7e75bb591755cc6f2e2e70bb894e6dd3a
estellechoi <estele.choi@gmail.com>
2019-07-15 15:35:03 +0900

    implements FocusListener

---
+++ java_tutorials/src/LessonB5_EventListener/implementClass_Focus.java
@@ -0,0 +1,37 @@
+package LessonB5_EventListener;
+
+import java.awt.FlowLayout;
+import java.awt.TextField;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import javax.swing.JFrame;
+
+public class implementClass_Focus extends JFrame implements FocusListener {
+
+	TextField tf1 = new TextField(10);
+	TextField tf2 = new TextField(10);
+
+	implementClass_Focus() {
+		tf1.addFocusListener(this);
+		add(tf1);
+		add(tf2);
+		setLayout(new FlowLayout());
+		setSize(200, 200);
+		setVisible(true);
+	}
+
+	@Override
+	public void focusGained(FocusEvent e) {
+		// = onfocus of javascript
+		System.out.print("focus gained");
+
+	}
+
+	@Override
+	public void focusLost(FocusEvent e) {
+		// = onblur of javascript
+		System.out.print("focus lost");
+
+	}
+
+}
---
+++ java_tutorials/src/LessonB5_EventListener/implementClass_Focus_main.java
@@ -0,0 +1,11 @@
+package LessonB5_EventListener;
+
+public class implementClass_Focus_main {
+
+	public static void main(String[] args) {
+
+		implementClass_Focus f = new implementClass_Focus();
+
+	}
+
+}

cf022854578966422aaa85943a394e66f1cea145
estellechoi <estele.choi@gmail.com>
2019-07-15 15:23:16 +0900

    implements MouseListener

---
+++ java_tutorials/src/LessonB5_EventListener/implementClass_mouse.java
@@ -0,0 +1,54 @@
+package LessonB5_EventListener;
+
+import java.awt.FlowLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
+public class implementClass_mouse extends JFrame implements MouseListener {
+	
+	JButton btn = new JButton("확인");
+	
+	implementClass_mouse() {
+		btn.addMouseListener(this);
+		add(btn);
+		setLayout(new FlowLayout());
+		setSize(300,200);
+		setVisible(true);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// = onmouseover of javascript
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// = onclick of javascript
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// = mouse released right after clicked
+		System.out.println("마우스 클릭");
+		
+	}
+	
+	
+
+}
---
+++ java_tutorials/src/LessonB5_EventListener/implementClass_mouse_main.java
@@ -0,0 +1,11 @@
+package LessonB5_EventListener;
+
+public class implementClass_mouse_main {
+
+	public static void main(String[] args) {
+
+		implementClass_mouse f = new implementClass_mouse();
+
+	}
+
+}

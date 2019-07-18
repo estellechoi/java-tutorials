a30120891606253a95483108bb8989e757fef95f
estellechoi <estele.choi@gmail.com>
2019-07-18 10:26:11 +0900

    ++a 와 a++ 변수 대입 시의 차이점

---
+++ java_tutorials/src/LessonA1_int/Unary.java
@@ -0,0 +1,18 @@
+package LessonA1_int;
+
+public class Unary {
+
+	public static void main(String[] args) {
+
+		int x = 1;
+		int y = 1;
+
+		int xx = ++x; // x = x+1 ¡æ xx = x+1
+		int yy = y++; // yy = y ¡æ y = y+1
+
+		System.out.println(x + " " + y);
+		System.out.println(xx + " " + yy);
+
+	}
+
+}

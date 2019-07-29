3e909a4269f9c1d1c518496e80c5f2ab263c7d6d
estellechoi <estele.choi@gmail.com>
2019-07-29 10:48:10 +0900

    배열 값을 낮은 순으로 정렬

--- java_tutorials/src/LessonA8_SortingAlgorithms/ArraySort.java
+++ java_tutorials/src/LessonA8_SortingAlgorithms/ArraySort.java
@@ -6,7 +6,7 @@ public class ArraySort {
 
 		int lotto[] = new int[4];
 
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < lotto.length; i++) {
 			lotto[i] = (int) (Math.random() * 45) + 1;
 		}
 
@@ -14,17 +14,17 @@ public class ArraySort {
 
 		int x; // 값 치환을 위한 임시 변수
 
-		for (int i = 0; i < 4; i++) {
-			for (int j = i + 1; j < 4; j++) {
+		for (int i = 0; i < lotto.length - 1; i++) {
+			for (int j = i + 1; j < lotto.length; j++) {
 				if (lotto[i] > lotto[j]) {
 					x = lotto[j];
 					lotto[j] = lotto[i];
 					lotto[i] = x;
 				}
-				
+
 			}
 			System.out.println(lotto[i]);
-			
+
 		}
 
 	}

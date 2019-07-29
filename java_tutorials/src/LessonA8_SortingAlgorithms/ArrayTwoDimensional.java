34958cdbc1bac2218326facd2dd82049cdd3acd4
estellechoi <estele.choi@gmail.com>
2019-07-29 11:39:38 +0900

    2차원 배열

---
+++ java_tutorials/src/LessonA8_SortingAlgorithms/ArrayTwoDimensional.java
@@ -0,0 +1,24 @@
+package LessonA8_SortingAlgorithms;
+
+// 2차원 배열
+public class ArrayTwoDimensional {
+
+	public static void main(String[] args) {
+
+		// 2차원 배열 선언
+		int arr[][];
+		arr = new int[5][5];
+
+		// 2차원 배열 선언과 생성
+		int a[][] = new int[5][5];
+
+		// 2차원 배열 선언과 초기화
+		// [ex] 3행 3열
+		int b[][] = { { 5, 7, 9 }, { 6, 2, 3 }, { 4, 3, 7 } }; // {{1행}, {2행}, {3행}, ...}
+
+		// 1차원 배열 선언과 초기화
+		int c[] = { 1, 2, 3, 4 };
+
+	}
+
+}

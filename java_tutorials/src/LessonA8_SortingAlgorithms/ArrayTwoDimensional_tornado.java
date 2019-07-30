1d9332bdd8f627126bbd6c4f984f54be760dee9a
estellechoi <estele.choi@gmail.com>
2019-07-30 11:37:37 +0900

    2차원 배열 골뱅이

---
+++ java_tutorials/src/LessonA8_SortingAlgorithms/ArrayTwoDimensional_tornado.java
@@ -0,0 +1,12 @@
+package LessonA8_SortingAlgorithms;
+
+// °ñ¹ðÀÌ Á¤·Ä
+public class ArrayTwoDimensional_tornado {
+
+	public static void main(String[] args) {
+		
+		int arr[][] = new int[5][5];
+
+	}
+
+}

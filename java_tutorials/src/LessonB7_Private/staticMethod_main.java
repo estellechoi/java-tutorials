5e532c372a757bff7c3f3d9d9390f8c40d98026f
estellechoi <estele.choi@gmail.com>
2019-08-06 09:40:07 +0900

    static method (정적 메소드) : 다른 클래스에서 객체 생성없이 호출 가능 (Class.method();)

--- java_tutorials/src/LessonB7_Private/staticMethod_main.java
+++ java_tutorials/src/LessonB7_Private/staticMethod_main.java
@@ -3,13 +3,15 @@ package LessonB7_Private;
 public class staticMethod_main {
 
 	public static void main(String[] args) {
-		
+
 		// call static method
 		int kor = 100;
 		staticMethod.cal(kor);
-		
+
 		// cannot call non-static one unless creating an object.
-		staticMethod.print();
+		staticMethod sm = new staticMethod();
+        // staticMethod.print();
+		sm.print();
 
 	}
 

524e15d910e1966f0fcf54e423c2913cdfcb78ea
estellechoi <estele.choi@gmail.com>
2019-08-08 10:16:49 +0900

    묵시적 생성자, 부모 생성자 호출 : super()

--- java_tutorials/src/LessonA4_Constructor/Cons3.java
+++ java_tutorials/src/LessonA4_Constructor/Cons3.java
@@ -3,17 +3,17 @@ package LessonA4_Constructor;
 // * 생성자(Constructor) : 모든 클래스의 객체 생성시 실행되는 메소드
 //   한 클래스 내에는 여러 개의 생성자 존재 (실행은 오직 하나만)
 //   생성자 생략 (기능 없는 생성자가 실행됨) → 묵시적 생성자 
-public class Cons3 {
+class parent {
 
 	int kor = 100;
 
 	public void print() {
-		System.out.println("Cons3");
+		System.out.println("parent");
 	}
 }
 
 // 클래스 상속
-class daughter extends Cons3 {
+class daughter extends parent {
 
 	int eng = 100;
 
@@ -22,17 +22,23 @@ class daughter extends Cons3 {
 	}
 	
 	daughter() {
-		super(); // 부모클래스의 생성자 먼저 실행시키기
+		
+		super();
+		// 부모클래스의 생성자 호출 (여기서는 묵시적 생성자)
+		// 가장 첫 줄에만 호출 가능 (부모 생성자는 항상 자식 생성자보다 먼저 실행되어야 함)
+		
+		System.out.println("daughter 생성자");
 	}
 }
 
 // 실행 클래스
-class son {
+// * public class의 이름은 파일명과 동일해야 함 *
+public class Cons3 {
 
 	public static void main(String[] args) {
 
 		// 객체 생성 (클래스 복사)
-		// 클래스 상속 중인 경우, 부모 생성자 실행 → 자식 새성자 실행
+		// 클래스 상속 중인 경우, 부모 생성자 실행 → 자식 생성자 실행
 		daughter d = new daughter();
 		System.out.println(d.kor);
 		d.print();

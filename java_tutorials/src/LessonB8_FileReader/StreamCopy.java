af50e423267f572f1c44c97ccdf6aa03f6359ad3
estellechoi <estele.choi@gmail.com>
2019-08-23 10:51:24 +0900

    보조스트림 BufferedInputStream/BufferedOutputStream 사용해 파일 읽고 쓰기 (a.k.a 복붙)

---
+++ java_tutorials/src/LessonB8_FileReader/StreamCopy.java
@@ -0,0 +1,30 @@
+package LessonB8_FileReader;
+
+import java.io.*;
+
+public class StreamCopy {
+
+	public static void main(String[] args) throws IOException {
+
+		FileInputStream fis = new FileInputStream("d:/d.txt");
+		BufferedInputStream bis = new BufferedInputStream(fis);
+		FileOutputStream fos = new FileOutputStream("d:/dd.txt");
+		BufferedOutputStream bos = new BufferedOutputStream(fos);
+
+		long start = System.currentTimeMillis();
+		int i;
+
+		while ((i = bis.read()) != -1) {
+			bos.write(i);
+		}
+
+		long end = System.currentTimeMillis();
+		System.out.println("걸린시간 : " + (end - start));
+
+		bis.close();
+		bos.close();
+		fis.close();
+		fos.close();
+	}
+
+}

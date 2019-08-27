e71a234345a4cdd3c4fec3b1cf4aaf38e3790b79
estellechoi <estele.choi@gmail.com>
2019-08-27 10:32:53 +0900

    배열을 리스트로 변환하기 / ArrayList의 상위 인터페이스 List li = Arrays.asList(array);

--- java_tutorials/src/LessonB9_Collections/Arrays_asList.java
+++ java_tutorials/src/LessonB9_Collections/Arrays_asList.java
@@ -1,6 +1,6 @@
 // 배열을 리스트로 변환하기 / ArrayList의 상위 인터페이스 List
 // List는 ArrayList, LinkedList 클래스의 상위 인터페이스
-package LessonB9_Collection;
+package LessonB9_Collections;
 
 import java.util.Arrays;
 import java.util.List;

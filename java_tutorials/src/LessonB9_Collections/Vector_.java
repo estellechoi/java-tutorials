02f7dfe7d938a0d84383cbb3d4948c7e17baa5d2
estellechoi <estele.choi@gmail.com>
2019-08-27 10:44:19 +0900

    Vector 로 데이터 추가하기 / 속도가 느려 잘 사용하지 않음

---
+++ java_tutorials/src/LessonB9_Collections/Vector_.java
@@ -0,0 +1,22 @@
+// * Vector는 자바 1.0 부터 사용하던 클래스 (속도 느림)
+// * Vector의 장점은 스레드 사용시 유리함
+package LessonB9_Collections;
+
+import java.util.Vector;
+
+public class Vector_ {
+
+	public static void main(String[] args) {
+
+		Vector<String> vc = new Vector<String>();
+		vc.add("유진");
+		vc.add("용기");
+		System.out.println(vc);
+
+		// * 지정한 인덱스에 요소 추가하기
+		vc.add(1, "승국");
+		System.out.println(vc);
+
+	}
+
+}
